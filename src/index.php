844b6e759ae3ad8d95464d59ae7cdbd6ddfe364b
ghanemla <ghanem.lamloumi1@gmail.com>
2022-12-07 17:03:50 +0100

    fixed add task feature

--- src/index.php
+++ src/index.php
@@ -2,3 +2,24 @@
 include ('taskhandler.php');
 
 ?>
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>U04-To-Do-list</title>
+</head>
+<body>
+  <div class="container">
+     <h1>To-Do-List</h1>
+  <form action="index.php" method="POST" class="input-form">
+    <?php if (isset($err_msg)) { ?>
+	    <p><?php echo $err_msg; ?></p>
+    <?php } ?>
+    <input type="text" name="task" class="todos" placeholder="Add Task">
+    <button type="submit" name="submit" value="1" id="add-btn" class="add-btn"> Add &#x2b;</button>
+  </form>
+  </div>
+</body>
+</html>
--- src/taskhandler.php
+++ src/taskhandler.php
@@ -4,14 +4,31 @@ $username = "root";
 $password = "example";
 $db = "todo";
 
+$err_msg = '';
+
 //Checking connection to the data base
 try{
   $conn = new PDO("mysql:host=".$servername.";dbname=".$db, $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  echo "Connected!";
+  //echo "Connected!";
 }
 catch(PDOException $e){
   echo "Connection failed: ". $e->getMessage();
+}
+
+
+//add task
+if(isset($_POST['submit'])){
+  if (empty($_POST['task'])) {
+    $err_msg = "Please fill in the task you want to track!";
+  }
+  else{
+    $task = $_POST['task'];
+    $sql = "INSERT INTO task (todos) VALUES (?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$task]);
+    header('location: index.php');
+  }
 }
 ?>
