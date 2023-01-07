3289fef317aebc64c6fa13f216dfaf0fcb80e3b5
Ghanem Lamloumi <42250752+Ghanemla@users.noreply.github.com>
2023-01-07 13:06:25 +0100

    Update db.sql

--- databas/db.sql
+++ databas/db.sql
@@ -13,7 +13,7 @@ USE `todo`;
 
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
-  `id` int nNOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `todos` varchar(255) NOT NULL,
   `description` TEXT NOT NULL,
   PRIMARY KEY (`id`)
