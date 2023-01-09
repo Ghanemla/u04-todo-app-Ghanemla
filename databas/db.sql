922abcec42d3f6271807a3fd036faa0cd8353b65
Ghanem Lamloumi <42250752+Ghanemla@users.noreply.github.com>
2023-01-09 08:43:59 +0100

    Update db.sql

--- databas/db.sql
+++ databas/db.sql
@@ -15,9 +15,8 @@ DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
   `id` int NOT NULL AUTO_INCREMENT,
   `todos` varchar(255) NOT NULL,
-  `description` TEXT NOT NULL,
+  `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
--- 2022-11-13 22:11:35
+-- 2023-01-08 12:51:09
