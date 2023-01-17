3525b2fa3de47ef4b7a55a8df3aa88603c1c9b78
Ghanemla <Ghanem.lamloumi1@gmail.com>
2023-01-17 13:07:49 +0100

    database structure

--- databas/db.sql
+++ databas/db.sql
@@ -7,16 +7,30 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP DATABASE IF EXISTS `todo`;
-CREATE DATABASE `todo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `todo`;
+DROP DATABASE IF EXISTS `todoapp`;
+CREATE DATABASE `todoapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `todoapp`;
 
-DROP TABLE IF EXISTS `task`;
-CREATE TABLE `task` (
+DROP TABLE IF EXISTS `todo_items`;
+CREATE TABLE `todo_items` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `todos` varchar(255) NOT NULL,
+  `task` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  `is_done` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `todo_items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 2023-01-08 12:51:09
+
+-- 2023-01-13 07:41:35
