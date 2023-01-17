43740990ef7465090806e0ee565a705550fa7630
Ghanemla <Ghanem.lamloumi1@gmail.com>
2023-01-17 13:45:26 +0100

    delete functionality

---
+++ src/deletetask.php
@@ -0,0 +1,23 @@
+<?php
+include('.//includes/dbconn.php');
+
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+  header('Location: login.php');
+  exit;
+}
+
+
+// Get the to-do item information
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  // $user_id = $_SESSION['user_id'];
+  $stmt = $conn->prepare("DELETE FROM todo_items WHERE id = :id AND user_id = :user_id");
+  $stmt->bindValue(':id', $id);
+  $stmt->bindValue(':user_id', $_SESSION['user_id']);
+  $stmt->execute();
+  // echo "cum";
+  header('Location: todo.php');
+}
